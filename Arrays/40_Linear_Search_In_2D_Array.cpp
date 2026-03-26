ec2b2ef7e8e578ce55054302c66a861f96d4116a
aanchal2006-dubey <aanchalddubey@gmail.com>
2026-03-26 18:09:41 +0530

    Solved Linear Search In 2D Array problem (Arrays)

---
+++ Arrays/40_Linear_Search_In_2D_Array.cpp
@@ -0,0 +1,26 @@
+/* Write a code to search element in 2D array.*/
+
+#include<iostream>
+using namespace std;
+
+int searchIn2DArray(int arr[][3], int rows, int cols, int key) {
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            if(arr[i][j] == key){
+                cout<<"Element found at position: "<<i<<", "<<j<<endl;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    int arr[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+    int rows = 4;
+    int cols = 3;
+
+    cout<<searchIn2DArray(arr, rows, cols, 8)<<endl;
+
+    return 0;
+}
